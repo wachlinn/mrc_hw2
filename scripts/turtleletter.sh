03913dd864d5c408af656d90b5f52363e80476ce
wachlinn <wachlinn@gmail.com>
2018-04-11 11:50:18 -0700

    Adding ex1.txt, ex2.txt, and scripts to the repository

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,12 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 1.57]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -2.5]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[5.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, 2.5]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4, 0.0, 0.0]' '[0.0, 0.0, 0.0]'
+
+
+
+
+
+
